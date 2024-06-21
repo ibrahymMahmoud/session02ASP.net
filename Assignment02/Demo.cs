dcbd919244ed2c716458da7c3d4c8e1374a1cb8d
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-06-21 20:55:56 +0300

    different between compiler and interpreter

--- Assignment02/Demo.cs
+++ Assignment02/Demo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,11 @@ namespace Assignment02
 {
     internal class Demo
     {
+        #region different between compiler and interpreter
+        /*
+        compiler:- (c - C++ - c# - java )no syanths error compile the all code once
+        interpreter:- (python - js) compile the code line by line
+        */ 
+        #endregion
     }
 }
